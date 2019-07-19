96b0b38fc39e7082e3055752989637a31ec67b5a
Koson Trachu <koson.trachu@gmail.com>
2019-07-20 03:21:09 +0700

    update sketch

--- examples/GatewaySerial/GatewaySerial.ino
+++ examples/GatewaySerial/GatewaySerial.ino
@@ -61,7 +61,7 @@
 // Enable inclusion mode
 #define MY_INCLUSION_MODE_FEATURE
 // Enable Inclusion mode button on gateway
-//#define MY_INCLUSION_BUTTON_FEATURE
+#define MY_INCLUSION_BUTTON_FEATURE
 
 // Inverses behavior of inclusion button (if using external pullup)
 //#define MY_INCLUSION_BUTTON_EXTERNAL_PULLUP
@@ -69,25 +69,28 @@
 // Set inclusion mode duration (in seconds)
 #define MY_INCLUSION_MODE_DURATION 60
 // Digital pin used for inclusion mode button
-//#define MY_INCLUSION_MODE_BUTTON_PIN  3
+#define MY_INCLUSION_MODE_BUTTON_PIN  3
 
 // Set blinking period
 #define MY_DEFAULT_LED_BLINK_PERIOD 300
 
 // Inverses the behavior of leds
-//#define MY_WITH_LEDS_BLINKING_INVERSE
+// #define MY_WITH_LEDS_BLINKING_INVERSE
 
 // Flash leds on rx/tx/err
 // Uncomment to override default HW configurations
-//#define MY_DEFAULT_ERR_LED_PIN 4  // Error led pin
-//#define MY_DEFAULT_RX_LED_PIN  6  // Receive led pin
-//#define MY_DEFAULT_TX_LED_PIN  5  // the PCB, on board LED
+#define MY_DEFAULT_ERR_LED_PIN 4  // Error led pin
+#define MY_DEFAULT_RX_LED_PIN  6  // Receive led pin
+#define MY_DEFAULT_TX_LED_PIN  5  // the PCB, on board LED
 
 #include <MySensors.h>
 
 void setup()
 {
 	// Setup locally attached sensors
+	digitalWrite(6,0);
+	_delay_ms(10000);
+	digitalWrite(6,1);
 }
 
 void presentation()
@@ -97,5 +100,6 @@ void presentation()
 
 void loop()
 {
+	Serial.print("Battery Voltage: ");
 	// Send locally attached sensor data here
 }
